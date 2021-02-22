7f3e9f3134c41405aeba3c9d5343ba29e0342460
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-22 06:51:50 +0600

    tweets route add

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,9 +8,6 @@ const passport = require("passport");
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
-// router.get("/test", (req, res) => {
-//   res.json({ msg: "This is the user route" });
-// });
 
 router.get(
   "/current",
@@ -37,7 +34,7 @@ router.post("/register", (req, res) => {
       return res.status(400).json(errors);
     } else {
       const newUser = new User({
-        name: req.body.name, // before that was handle: req.body.handle
+        handle: req.body.handle, // before that was handle: req.body.handle
         email: req.body.email,
         password: req.body.password,
       });
@@ -74,21 +71,21 @@ router.post("/login", (req, res) => {
 
     bcrypt.compare(password, user.password).then((isMatch) => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        // res.json({ msg: "Success" });
 
-        //   const payload = {id: user.id, handle: user.handle, email: user.email};
+          const payload = {id: user.id, handle: user.handle, email: user.email};
 
-        //   jwt.sign(
-        //       payload,
-        //       keys.secretOrKey,
-        //       {expiresIn: 3600},
-        //       (err, token) => {
-        //           res.json({
-        //               success: true,
-        //               token: 'Bearer ' + token
-        //           });
-        //       }
-        //   )
+          jwt.sign(
+              payload,
+              keys.secretOrKey,
+              {expiresIn: 3600},
+              (err, token) => {
+                  res.json({
+                      success: true,
+                      token: 'Bearer ' + token
+                  });
+              }
+          )
       } else {
         errors.password = "Incorrect password";
         return res.status(400).json(errors);
@@ -98,89 +95,5 @@ router.post("/login", (req, res) => {
   });
 });
 
-// router.post("/register", (req, res) => {
-//   const { errors, isValid } = validateRegisterInput(req.body);
-
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
-
-//   User.findOne({ handle: req.body.handle }).then((user) => {
-//     if (user) {
-//       errors.handle = "User already exists";
-//       return res.status(400).json(errors);
-//     } else {
-//       const newUser = new User({
-//         handle: req.body.handle,
-//         email: req.body.email,
-//         password: req.body.password,
-//       });
-
-//       bcrypt.genSalt(10, (err, salt) => {
-//         bcrypt.hash(newUser.password, salt, (err, hash) => {
-//           if (err) throw err;
-//           newUser.password = hash;
-//           newUser
-//             .save()
-//             .then((user) => {
-//               const payload = { id: user.id, handle: user.handle };
-
-//               jwt.sign(
-//                 payload,
-//                 keys.secretOrKey,
-//                 { expiresIn: 3600 },
-//                 (err, token) => {
-//                   res.json({
-//                     success: true,
-//                     token: "Bearer " + token,
-//                   });
-//                 }
-//               );
-//             })
-//             .catch((err) => console.log(err));
-//         });
-//       });
-//     }
-//   });
-// });
-
-// router.post("/login", (req, res) => {
-//   const { errors, isValid } = validateLoginInput(req.body);
-
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
-
-//   const handle = req.body.handle;
-//   const password = req.body.password;
-
-//   User.findOne({ handle }).then((user) => {
-//     if (!user) {
-//       errors.handle = "This user does not exist";
-//       return res.status(400).json(errors);
-//     }
-
-//     bcrypt.compare(password, user.password).then((isMatch) => {
-//       if (isMatch) {
-//         const payload = { id: user.id, handle: user.handle };
-
-//         jwt.sign(
-//           payload,
-//           keys.secretOrKey,
-//           { expiresIn: 3600 },
-//           (err, token) => {
-//             res.json({
-//               success: true,
-//               token: "Bearer " + token,
-//             });
-//           }
-//         );
-//       } else {
-//         errors.password = "Incorrect password";
-//         return res.status(400).json(errors);
-//       }
-//     });
-//   });
-// });
 
 module.exports = router;
